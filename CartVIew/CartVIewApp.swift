014a1a5c38d48b1ec68bb98c25e94af9e48771c4
Achmad Ichsan Fauzi <ichsan.fauzi@icloud.com>
2020-10-26 00:22:09 +0700

    Initial Commit

---
+++ CartVIew/CartVIewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartVIewApp.swift
+//  CartVIew
+//
+//  Created by Achmad Ichsan Fauzi on 26/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CartVIewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
